ff59fbd32bb77e76aea428a6ea3a49b5f3b5cd04
Artem Grotov <agrotov@gmail.com>
2016-03-16 16:59:04 +0100

    verbose

--- opts.lua
+++ opts.lua
@@ -40,6 +40,7 @@ function M.parse(arg)
     cmd:option('-netType',     'alexnetowtbn', 'Options: alexnet | overfeat | alexnetowtbn | vgg | googlenet')
     cmd:option('-retrain',     'none', 'provide path to model to retrain with')
     cmd:option('-optimState',  'none', 'provide path to an optimState to reload from')
+    cmd:option('-verbose',  true, 'verbose output')
     cmd:text()
 
     local opt = cmd:parse(arg or {})
