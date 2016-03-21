407c0600520fa01ca4531ad6cc17105eec388152
Artem Grotov <agrotov@gmail.com>
2016-03-21 13:59:38 +0100

    bandit

---
+++ load_csv.lua
@@ -0,0 +1,54 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: agrotov
+-- Date: 3/21/16
+-- Time: 1:56 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+
+function load_rewards_csv(filePath)
+    -- Read CSV file
+
+    -- Split string
+    function string:split(sep)
+      local sep, fields = sep, {}
+      local pattern = string.format("([^%s]+)", sep)
+      self:gsub(pattern, function(substr) fields[#fields + 1] = substr end)
+      return fields
+    end
+
+
+    -- Count number of rows and columns in file
+    local i = 0
+    for line in io.lines(filePath) do
+      if i == 0 then
+        COLS = #line:split(',')
+      end
+      i = i + 1
+    end
+
+    ROWS = i
+   --local ROWS = i - 1  -- Minus 1 because of header
+
+    -- Read data from CSV to tensor
+    local csvFile = io.open(filePath, 'r')
+    local header = csvFile:read()
+
+    local data = torch.Tensor(ROWS, COLS)
+
+    local i = 0
+    for line in csvFile:lines('*l') do
+      i = i + 1
+      local l = line:split(',')
+      for key, val in ipairs(l) do
+        data[i][key] = val
+      end
+    end
+
+    csvFile:close()
+
+    return data
+end
+
+
--- train_bandit.lua
+++ train_bandit.lua
@@ -12,6 +12,54 @@ autograd = require 'autograd'
 
 
 
+
+
+function load_rewards_csv(filePath)
+    -- Read CSV file
+
+    -- Split string
+    function string:split(sep)
+      local sep, fields = sep, {}
+      local pattern = string.format("([^%s]+)", sep)
+      self:gsub(pattern, function(substr) fields[#fields + 1] = substr end)
+      return fields
+    end
+
+
+    -- Count number of rows and columns in file
+    local i = 0
+    for line in io.lines(filePath) do
+      if i == 0 then
+        COLS = #line:split(',')
+      end
+      i = i + 1
+    end
+
+    ROWS = i
+   --local ROWS = i - 1  -- Minus 1 because of header
+
+    -- Read data from CSV to tensor
+    local csvFile = io.open(filePath, 'r')
+    local header = csvFile:read()
+
+    local data = torch.Tensor(ROWS, COLS)
+
+    local i = 0
+    for line in csvFile:lines('*l') do
+      i = i + 1
+      local l = line:split(',')
+      for key, val in ipairs(l) do
+        data[i][key] = val
+      end
+    end
+
+    csvFile:close()
+
+    return data
+end
+
+
+
 local function sample_action(model_output)
     local result = torch.multinomial(model_output,1)
     return result
@@ -136,7 +184,7 @@ function train()
    -- set the dropouts to training mode
    model:training()
 
-   loss_matrix = load_rewards("/home/agrotov/imagenet-multiGPU.torch/loss_matrix.txt")
+   loss_matrix = load_rewards_csv("/home/agrotov/imagenet-multiGPU.torch/loss_matrix.txt")
 
    local tm = torch.Timer()
    top1_epoch = 0
