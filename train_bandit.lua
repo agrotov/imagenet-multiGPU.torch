56509bb63e0ec8cbd76aadeff94f67cb802591d7
Artem Grotov <agrotov@gmail.com>
2016-03-21 14:25:20 +0100

    bandit

--- train_bandit.lua
+++ train_bandit.lua
@@ -268,8 +268,8 @@ function trainBatch(inputsCPU, labelsCPU)
 
 
       actions = sample_action(outputs)
-      p_of_actions = probability_of_actions(outputs, actions)
-      rewards = reward_for_actions(loss_matrix, actions, labels)
+      --p_of_actions = probability_of_actions(outputs, actions)
+      --rewards = reward_for_actions(loss_matrix, actions, labels)
       --target = compute_target()
 
       return err, gradParameters
