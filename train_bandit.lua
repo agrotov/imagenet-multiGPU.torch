9e10870383192f49291ed3a805df38a477b46760
Artem Grotov <agrotov@gmail.com>
2016-03-21 15:55:22 +0100

    bandit

--- train_bandit.lua
+++ train_bandit.lua
@@ -97,7 +97,7 @@ local function reward_for_actions(loss_matrix, actions, labels)
 
 --
 
-    result = loss_matrix:index(1,actions:view(actions:nElement())):gather(2,labels:long())
+    result = loss_matrix:index(1,actions:view(actions:nElement())):gather(1,labels:long())
 --    print("result")
 --    print(result)
 
