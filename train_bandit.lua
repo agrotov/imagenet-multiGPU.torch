02e48d7c78f9a143d7784f2c44ae164b9d71fec2
Artem Grotov <agrotov@gmail.com>
2016-03-23 13:26:52 +0100

    bandit

--- train_bandit.lua
+++ train_bandit.lua
@@ -298,8 +298,8 @@ function trainBatch(inputsCPU, labelsCPU)
 
       local size_output = outputs:size()
       local actions = sample_action(outputs)
-      print("actions")
-      print(actions)
+      print("outputs")
+      print(outputs)
 
       local p_of_actions_teacher = probability_of_actions(outputs, actions)
       local p_of_actions_student = probability_of_actions(outputs, actions)
