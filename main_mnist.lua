392bb8ed969357acd02d8cef4027f41329c1b80a
Artem Grotov <agrotov@gmail.com>
2016-04-28 01:26:43 +0200

    mnist

--- main_mnist.lua
+++ main_mnist.lua
@@ -207,7 +207,7 @@ function train_mnist_bandit(dataset)
       }
 
       outputs = trainBatch(inputs,targets, optimState)
-      print(outputs)
+--      print(outputs)
       -- disp progress
       xlua.progress(t, dataset:size())
 
