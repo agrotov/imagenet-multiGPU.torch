d494db28c62c99e1bf92d123fe426a2b66dba2b1
Artem Grotov <agrotov@gmail.com>
2016-04-27 15:53:13 +0200

    mnist

--- train.lua
+++ train.lua
@@ -174,21 +174,21 @@ function trainBatch(inputsCPU, labelsCPU, optimState)
    cutorch.synchronize()
    batchNumber = batchNumber + 1
    -- top-1 error
-   local top1 = 0
-   do
-      local _,prediction_sorted = outputs:float():sort(2, true) -- descending
-      for i=1,opt.batchSize do
-	 if prediction_sorted[i][1] == labelsCPU[i] then
-	    top1_epoch = top1_epoch + 1;
-	    top1 = top1 + 1
-	 end
-      end
-      top1 = top1 * 100 / opt.batchSize;
-   end
-   -- Calculate top-1 error, and print information
-   print(('Epoch: [%d][%d/%d]\tTime %.3f Err %.4f Top1-%%: %.2f LR %.0e DataLoadingTime %.3f'):format(
-          epoch, batchNumber, opt.epochSize, timer:time().real, err, top1,
-          optimState.learningRate, dataLoadingTime))
-
+--   local top1 = 0
+--   do
+--      local _,prediction_sorted = outputs:float():sort(2, true) -- descending
+--      for i=1,opt.batchSize do
+--	 if prediction_sorted[i][1] == labelsCPU[i] then
+--	    top1_epoch = top1_epoch + 1;
+--	    top1 = top1 + 1
+--	 end
+--      end
+--      top1 = top1 * 100 / opt.batchSize;
+--   end
+--   -- Calculate top-1 error, and print information
+--   print(('Epoch: [%d][%d/%d]\tTime %.3f Err %.4f Top1-%%: %.2f LR %.0e DataLoadingTime %.3f'):format(
+--          epoch, batchNumber, opt.epochSize, timer:time().real, err, top1,
+--          optimState.learningRate, dataLoadingTime))
+--
    dataTimer:reset()
 end
