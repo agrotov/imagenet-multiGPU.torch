365594d309dee17585ca84630b30b657c5bbb30e
Artem Grotov <agrotov@gmail.com>
2016-04-27 16:02:23 +0200

    mnist

--- main_mnist.lua
+++ main_mnist.lua
@@ -202,30 +202,39 @@ function train_mnist_bandit(dataset)
       trainBatch(inputs,targets, optimState)
 
       -- disp progress
---      xlua.progress(t, dataset:size())
+      xlua.progress(t, dataset:size())
 
    end
 
+
+
+   -- update confusion
+   for i = 1,opt.batchSize do
+      confusion:add(outputs[i], targets[i])
+   end
+
+
    -- time taken
---   time = sys.clock() - time
---   time = time / dataset:size()
---   print("<trainer> time to learn 1 sample = " .. (time*1000) .. 'ms')
---
---   -- print confusion matrix
---   print(confusion)
---   trainLogger:add{['% mean class accuracy (train set)'] = confusion.totalValid * 100}
---   confusion:zero()
---
---   -- save/log current net
---   local filename = paths.concat(opt.save, 'mnist.net')
---   os.execute('mkdir -p ' .. sys.dirname(filename))
---   if paths.filep(filename) then
---      os.execute('mv ' .. filename .. ' ' .. filename .. '.old')
---   end
---   print('<trainer> saving network to '..filename)
---   -- torch.save(filename, model)
---
---   -- next epoch
+
+   time = sys.clock() - time
+   time = time / dataset:size()
+   print("<trainer> time to learn 1 sample = " .. (time*1000) .. 'ms')
+
+   -- print confusion matrix
+   print(confusion)
+   trainLogger:add{['% mean class accuracy (train set)'] = confusion.totalValid * 100}
+   confusion:zero()
+
+   -- save/log current net
+   local filename = paths.concat(opt.save, 'mnist.net')
+   os.execute('mkdir -p ' .. sys.dirname(filename))
+   if paths.filep(filename) then
+      os.execute('mv ' .. filename .. ' ' .. filename .. '.old')
+   end
+   print('<trainer> saving network to '..filename)
+   -- torch.save(filename, model)
+
+   -- next epoch
    epoch = epoch + 1
 end
 
