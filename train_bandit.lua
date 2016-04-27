4f86fd8df884068e398decb69dffe85ad5210d0e
Artem Grotov <agrotov@gmail.com>
2016-04-28 01:52:03 +0200

    mnist

--- train_bandit.lua
+++ train_bandit.lua
@@ -68,7 +68,7 @@ end
 
 local function sample_action(model_output)
 --    return torch.multinomial(model_output,1):long()
-    return torch.Tensor(1,1):fill(torch.random(0,10))
+    return torch.Tensor(1,1):fill(torch.random(0,10)):long()
 end
 
 
