e5ebb8183e3ab83af3acf34d38430d58c0fcd4b3
Artem Grotov <agrotov@gmail.com>
2016-04-28 02:49:37 +0200

    mnist

--- main_mnist.lua
+++ main_mnist.lua
@@ -134,9 +134,10 @@ cudnn.convert(model, cudnn)
 criterion = nn.ClassNLLCriterion()
 criterion:cuda()
 
+
 paths.dofile('train.lua')
 --paths.dofile('train_bandit.lua')
-loss_matrix = load_rewards_mnist()
+--loss_matrix = load_rewards_mnist()
 
 
 print(model)
