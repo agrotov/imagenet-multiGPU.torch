545f1cb4c380bc5369a9509a7245e64f8505eb94
Artem Grotov <agrotov@gmail.com>
2016-04-28 05:36:52 +0200

    mnist

--- main_mnist.lua
+++ main_mnist.lua
@@ -322,7 +322,7 @@ while true do
    train_mnist_bandit(trainData)
    if (epoch > 1) then
       save_bandit_dataset("/var/scratch/agrotov/bandit_mnist/mnist_bandit_dataset")
-      print(bandit_dataset)
+--      print(bandit_dataset)
       exit()
    end
 
