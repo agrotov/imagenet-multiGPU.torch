fe198f0126251abf3fb4640a60abfaa3d12132d7
Artem Grotov <agrotov@gmail.com>
2016-04-28 04:06:53 +0200

    mnist

--- train_bandit.lua
+++ train_bandit.lua
@@ -69,7 +69,7 @@ end
 function sample_action(model_output, temperature)
     temperature = temperature or 1
 --    print(model_output)
-    result =  torch.multinomial(torch.exp(model_output/temperature),1):long()
+    result =  torch.multinomial(torch.exp(model_output/10),1):long()
 --    result = torch.Tensor(10,1):random(1,10):long()
 --    print(result)
     return result
