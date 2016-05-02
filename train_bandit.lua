91182d47aa13b2d5bf30fba62cdb452b0e09cd09
Artem Grotov <agrotov@gmail.com>
2016-05-03 01:06:46 +0200

    mnist

--- train_bandit.lua
+++ train_bandit.lua
@@ -404,7 +404,8 @@ function trainBatch_bandit(inputsCPU, actions_cpu, rewards_cpu, probabilities_lo
    do
       local _,prediction_sorted = outputs:float():sort(2, true) -- descending
       for i=1,opt.batchSize do
-         if prediction_sorted[i][1] == labelsCPU[i] then
+--         if prediction_sorted[i][1] == labelsCPU[i] then
+        if actions[i] == labelsCPU[i] then
             top1_epoch = top1_epoch + 1;
             top1 = top1 + 1
          end
