aae92e0301599b2ae240d676e01bebd167ea96bb
Artem Grotov <agrotov@gmail.com>
2016-05-17 14:03:29 +0200

    imagenet

--- main_bandit.lua
+++ main_bandit.lua
@@ -109,7 +109,8 @@ function train_imagenet_bandit(model, data_path)
    -- local vars
    local time = sys.clock()
 
-   model:evaluate()
+--   model:evaluate()
+   model:training()
 
    temperature = 0.03
 
