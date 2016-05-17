896ee7d216d77e58e33c587c0e2e7c2d942dd303
Artem Grotov <agrotov@gmail.com>
2016-05-17 14:06:51 +0200

    imagenet

--- main_bandit.lua
+++ main_bandit.lua
@@ -147,7 +147,7 @@ function train_imagenet_bandit(model, data_path)
 --         print(class)
 --         print(index_of_image)
 --         print(index_of_input)
-         local input, h1, w1, flip, index_tmp = trainLoader:getByClassAndIndex(class, index_of_image,h1, w1, flip)
+         local input, h1, w1, flip, index_tmp = trainLoader:getByClassAndIndex(class, index_of_image)
          targets[k] = class
          inputs[k] = input
          actions[k] = action
