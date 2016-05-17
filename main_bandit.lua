fb6fc225392b6a19fbd065b8424824819c34ef9c
Artem Grotov <agrotov@gmail.com>
2016-05-17 11:01:34 +0200

    imagenet

--- main_bandit.lua
+++ main_bandit.lua
@@ -60,7 +60,7 @@ function produce_dataset(model, data_path)
 
 --   opt.epochSize = 1
 
-   temperature = 0.05
+   temperature = 0.01
 
    for i=1,opt.epochSize do
 --      local inputs, labels, indexes = trainLoader:sample(opt.batchSize)
@@ -174,8 +174,8 @@ end -- of train_imagenet_bandit()
 
 
 data_path = "/var/scratch/agrotov/bandit_imagenet/logged_dataset_new_tiny"
---produce_dataset(model, data_path)
-train_imagenet_bandit(model,data_path)
+produce_dataset(model, data_path)
+--train_imagenet_bandit(model,data_path)
 
 
 --epoch = opt.epochNumber
