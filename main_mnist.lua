ab9de3ccfa13ae0968637d405c229c36a50214f5
Artem Grotov <agrotov@gmail.com>
2016-05-17 15:13:17 +0200

    imagenet

--- main_mnist.lua
+++ main_mnist.lua
@@ -259,7 +259,7 @@ function train_mnist_bandit(dataset,logged_data)
    -- local vars
    local time = sys.clock()
 
-   temperature = 10
+   temperature = 0.1
 
    -- do one epoch
    print('<trainer> on training set:')
