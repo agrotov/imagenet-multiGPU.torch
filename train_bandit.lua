1181b9e89972c468f2af578ebef5ed037760b2e9
Artem Grotov <agrotov@gmail.com>
2016-05-17 16:27:18 +0200

    imagenet

--- train_bandit.lua
+++ train_bandit.lua
@@ -399,7 +399,7 @@ function trainBatch_bandit(inputsCPU, actions_cpu, rewards_cpu, probabilities_lo
       return err, gradParameters
    end
 
---   optim.sgd(feval, parameters, optimState)
+   optim.sgd(feval, parameters, optimState)
 
    -- DataParallelTable's syncParameters
    if model.needsSync then
