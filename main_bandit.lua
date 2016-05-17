a3809fe97e4d84edea4b937e83f2e28dcf24018a
Artem Grotov <agrotov@gmail.com>
2016-05-17 13:45:36 +0200

    imagenet

--- main_bandit.lua
+++ main_bandit.lua
@@ -186,8 +186,8 @@ end -- of train_imagenet_bandit()
 
 
 data_path = "/var/scratch/agrotov/bandit_imagenet/logged_dataset_new_tiny"
---produce_dataset(model, data_path)
-train_imagenet_bandit(model,data_path)
+produce_dataset(model, data_path)
+--train_imagenet_bandit(model,data_path)
 
 
 --epoch = opt.epochNumber
