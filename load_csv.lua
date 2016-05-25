534076e7bff214e8cad1655f2beb3c1ee356cfbc
Artem Grotov <agrotov@gmail.com>
2016-05-25 13:57:03 +0200

    imagenet

--- load_csv.lua
+++ load_csv.lua
@@ -27,12 +27,13 @@ function load_rewards_csv(filePath)
       end
       i = i + 1
     end
-    print("load_rewards_csv")
+    print("load_rewards_csvload_rewards_csvload_rewards_csv")
     ROWS = i
    --local ROWS = i - 1  -- Minus 1 because of header
 
     -- Read data from CSV to tensor
     local csvFile = io.open(filePath, 'r')
+    print("csvFilecsvFilecsvFilecsvFile")
 --    local header = csvFile:read()
 
     local data = torch.Tensor(ROWS, COLS)
