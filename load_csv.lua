b7fd6311d0a688508e9577e4020d1a86ee231c57
Artem Grotov <agrotov@gmail.com>
2016-06-01 13:42:14 +0200

    imagenet

--- load_csv.lua
+++ load_csv.lua
@@ -14,7 +14,6 @@ function load_rewards_csv_new(filePath)
       local sep, fields = sep, {}
       local pattern = string.format("([^%s]+)", sep)
       self:gsub(pattern, function(substr) fields[#fields + 1] = substr end)
-      print("splitsplitsplitsplit")
       return fields
     end
 
@@ -27,18 +26,14 @@ function load_rewards_csv_new(filePath)
       end
       i = i + 1
     end
-    print("load_rewards_csvload_rewards_csvload_rewards_csv")
     ROWS = i
    --local ROWS = i - 1  -- Minus 1 because of header
 
     -- Read data from CSV to tensor
     local csvFile = io.open(filePath, 'r')
-    print("csvFilecsvFilecsvFilecsvFile")
 --    local header = csvFile:read()
 
     local data = torch.Tensor(ROWS, COLS)
-    print("datadatadatadata")
-    exit()
 
     local i = 0
     for line in csvFile:lines('*l') do
