9e1037b19de3d4dc80ace0c0b6bcb725dec6fd4b
Artem Grotov <agrotov@gmail.com>
2016-06-03 12:03:01 +0200

    imagenet

--- main_bandit.lua
+++ main_bandit.lua
@@ -171,7 +171,7 @@ function train_imagenet_bandit(model, data_path)
 --      print("probability_of_actions")
 --      print(probability_of_actions)
       print(actions)
-      exit()
+--      exit()
       outputs = trainBatch_bandit(inputs,actions,rewards,probability_of_actions, optimState, targets, temperature)
 
 --      exit()
