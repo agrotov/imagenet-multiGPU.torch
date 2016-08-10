3d29b6f0179e0b723fa512f56773f27ef4ed8fa7
Artem Grotov <agrotov@vpn-staff-146-50-69-29.vpn.uva.nl>
2016-08-10 13:14:01 +0300

    test

--- main_bandit.lua
+++ main_bandit.lua
@@ -206,11 +206,85 @@ function train_imagenet_bandit(model, data_path)
 end -- of train_imagenet_bandit()
 
 
+function test_imagenet_bandit(model, data_path)
+
+   paths.dofile('donkey.lua')
+
+   logged_data = torch.load(data_path)
+
+   loss_matrix = load_rewards_csv("/home/agrotov1/imagenet-multiGPU.torch/loss_matrix.txt")
+
+   epoch = epoch or 1
+   -- local vars
+   local time = sys.clock()
+
+--   model:evaluate()
+   model:training()
+
+   print("baseline",baseline)
+
+   temperature = 0.03
+
+   for t = 1,logged_data:size(1),opt.batchSize do
+
+      -- create mini batch
+      local inputs = torch.Tensor(opt.batchSize,3,opt.cropSize,opt.cropSize)
+      local actions = torch.Tensor(opt.batchSize)
+      local rewards = torch.Tensor(opt.batchSize)
+      local probability_of_actions = torch.Tensor(opt.batchSize)
+      local targets = torch.Tensor(opt.batchSize)
+
+      local k = 1
+      indexes = torch.Tensor(opt.batchSize,1)
+      for i = t,math.min(t+opt.batchSize-1,logged_data:size(1)) do
+--         print("i",i,"t",t)
+         local index_of_input = logged_data[i][1]
+         local action = logged_data[i][2]
+         local reward = logged_data[i][3]
+         local probability_of_action = logged_data[i][4]
+
+         local h1 = logged_data[i][5]
+         local w1 = logged_data[i][6]
+         local flip = logged_data[i][7]
+
+
+         -- load new sample
+         local class = ((index_of_input)%1001)
+         local index_of_image = math.floor((index_of_input/1001))
+         local input, h1, w1, flip, index_tmp = trainLoader:getByClassAndIndex(class, index_of_image, h1, w1, flip)
+         targets[k] = class
+         inputs[k] = input
+         actions[k] = action
+         rewards[k] = reward
+         probability_of_actions[k] = probability_of_action
+         k = k + 1
+      end
+
+--      opt.learningRate = 0.01
+
+      cutorch.synchronize()
+      optimState = sgdState or {
+         learningRate = opt.LR,
+         momentum = opt.momentum,
+         learningRateDecay = 5e-7
+      }
+      full_information_test(inputs, targets,batch_number, rewards)
+      batch_number = batch_number + 1
+   end
+end -- of test_imagenet_bandit()
+
+
+
+
+
+
+
 
 data_path = "/home/agrotov1/bandit_imagenet/logged_dataset_with_offsets"
 --produce_dataset(model, data_path)
 --print_bandit_dataset()
-train_imagenet_bandit(model,data_path)
+--train_imagenet_bandit(model,data_path)
+test_imagenet_bandit(model, data_path)
 --
 
 --epoch = opt.epochNumber
