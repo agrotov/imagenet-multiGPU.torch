6f0acc2002901bc9970ec7c5222ec85d3e86f797
Artem Grotov <agrotov@vpn-staff-146-50-69-29.vpn.uva.nl>
2016-08-13 20:54:46 +0300

    test

--- main_bandit.lua
+++ main_bandit.lua
@@ -281,7 +281,7 @@ end -- of test_imagenet_bandit()
 
 
 
-data_path = "/home/agrotov1/bandit_imagenet/logged_dataset_with_offsets_model_7.t7"
+data_path = "/home/agrotov1/bandit_imagenet/logged_dataset_with_offsets_model_7.t7_1000"
 produce_dataset(model, data_path)
 --print_bandit_dataset()
 --train_imagenet_bandit(model,data_path)
