4b7e0bfe02e4ac2e3119f3958a6e63a77d3699f0
Artem Grotov <agrotov@vpn-staff-146-50-68-161.vpn.uva.nl>
2016-08-15 22:07:32 +0300

    bandit

--- train_bandit.lua
+++ train_bandit.lua
@@ -271,7 +271,7 @@ function trainBatch_bandit(inputsCPU, actions_cpu, rewards_cpu, probabilities_lo
 
         err = rewards:mean()
         --model:backward(inputs, gpu_target)
-        model:backward(inputs, 0)
+        model:backward(inputs, outputs * 0)
 
         gradParameters:clamp(-5, 5)
 
