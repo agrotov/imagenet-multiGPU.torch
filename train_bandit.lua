0403266c935354cd5aa4acda0996a39422ef4af4
Artem Grotov <agrotov@vpn-staff-146-50-68-161.vpn.uva.nl>
2016-08-15 22:28:33 +0300

    bandit

--- train_bandit.lua
+++ train_bandit.lua
@@ -274,6 +274,7 @@ function trainBatch_bandit(inputsCPU, actions_cpu, rewards_cpu, probabilities_lo
 
 
         err = rewards:mean()
+        print("target",target)
         model:backward(inputs, gpu_target)
         --model:backward(inputs, torch.zeros(outputs:size()):cuda())
         gradParameters:clamp(-5, 5)
