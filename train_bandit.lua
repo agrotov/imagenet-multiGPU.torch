1f5d7f8c0e303b80bd3141bcff052915b6166a4e
Artem Grotov <agrotov@vpn-staff-146-50-68-161.vpn.uva.nl>
2016-08-15 22:43:08 +0300

    bandit

--- train_bandit.lua
+++ train_bandit.lua
@@ -275,11 +275,11 @@ function trainBatch_bandit(inputsCPU, actions_cpu, rewards_cpu, probabilities_lo
 
         err = rewards:mean()
         --print("target",target)
-        --model:backward(inputs, gpu_target)
-        model:backward(inputs, torch.zeros(outputs:size()):cuda())
+        model:backward(inputs, gpu_target)
+        --model:backward(inputs, torch.zeros(outputs:size()):cuda())
         nan_mask = gradParameters:ne(gradParameters)
         print("sum nan ",torch.sum(nan_mask))
-        
+
         print("gradParameters",torch.mean(gradParameters),torch.max(gradParameters),torch.min(gradParameters))
         gradParameters:clamp(-5, 5)
         print("gradParameters",torch.mean(gradParameters),torch.max(gradParameters),torch.min(gradParameters))
