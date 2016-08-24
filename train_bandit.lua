1d08d1e8ecdc7cef0df588a6c463ba309e23d768
Artem Grotov <agrotov@vpn-staff-146-50-69-191.vpn.uva.nl>
2016-08-24 15:55:28 +0300

    bandit

--- train_bandit.lua
+++ train_bandit.lua
@@ -349,7 +349,7 @@ function trainBatch_bandit(inputsCPU, actions_cpu, rewards_cpu, probabilities_lo
         --print("target",target)
 --        model:backward(inputs, gpu_target)
         ones_t =  torch.ones(outputs:size()):cuda() * 0.0
-        model:backward(inputs, ones_t)
+--        model:backward(inputs, ones_t)
 --        err = 1
 --        print("new target",torch.ones(outputs:size()):cuda()+5)
 --        print()
