54b055c814f3953c891dd91f0e0fead69d1f56da
Artem Grotov <agrotov@vpn-staff-146-50-71-22.vpn.uva.nl>
2016-09-12 16:11:37 +0300

    bandit

--- materialize_dataset.lua
+++ materialize_dataset.lua
@@ -53,6 +53,7 @@ function materialize_dataset(input_indexes, inputsCPU, labelsCPU, path, temperat
 
     local actions= torch.LongTensor(opt.batchSize)
     local _,prediction_sorted = outputs:float():sort(2, true) -- descending
+    for i=1,opt.batchSize do
         actions[i] = prediction_sorted[i][1]
     end
 
