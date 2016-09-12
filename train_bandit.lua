d30957dee3e280f52ab14b17a57e4186c7218407
Artem Grotov <agrotov@vpn-staff-146-50-71-22.vpn.uva.nl>
2016-09-12 15:54:27 +0300

    bandit

--- train_bandit.lua
+++ train_bandit.lua
@@ -376,7 +376,9 @@ function full_information_test(inputsCPU, actions, rewards_logged, probabilities
 
 --    print("actions_eva",actions_eva)
 
-    rewards_eva = 1-reward_for_actions(loss_matrix, actions_eva, labelsCPU)
+    local actions_sample = sample_action(outputs,1)
+
+    rewards_eva = 1-reward_for_actions(loss_matrix, actions_sample, labelsCPU)
 
     diff_rewards = rewards_eva:mean() - rewards_logged:mean()
 
