1fb991be3414ac3839bfdaa54df7bc570cb7e1d1
Artem Grotov <agrotov@vpn-staff-146-50-68-34.vpn.uva.nl>
2016-09-23 15:26:16 +0300

    bandit

--- main_bandit.lua
+++ main_bandit.lua
@@ -260,7 +260,7 @@ if opt.produce_dataset == 1 then
 end
 
 if opt.produce_test_dataset == 1 then
-    produce_dataset(model, data_path, testnLoader)
+    produce_dataset(model, data_path, testLoader)
 --    print_bandit_dataset()
 end
 
