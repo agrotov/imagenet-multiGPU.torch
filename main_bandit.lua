a4046bed50780a8b046d27b11907cf4e736eb0f8
Artem Grotov <agrotov@vpn-staff-146-50-70-160.vpn.uva.nl>
2016-09-28 13:28:07 +0300

    bandit

--- main_bandit.lua
+++ main_bandit.lua
@@ -105,9 +105,7 @@ function train_imagenet_bandit(model, data_path)
 
 
    logged_data = torch.load(data_path)
-
-   print("logged_data train_imagenet_bandit",logged_data)
-
+   
    loss_matrix = load_rewards_csv_new("/home/agrotov1/imagenet-multiGPU.torch/loss_matrix.txt")
 
    epoch = epoch or 1
