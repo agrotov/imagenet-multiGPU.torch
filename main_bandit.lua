222c57d5c7f56b967f04c55049eca614e6b9c39d
Artem Grotov <agrotov@vpn-staff-146-50-70-160.vpn.uva.nl>
2016-09-28 13:38:34 +0300

    bandit

--- main_bandit.lua
+++ main_bandit.lua
@@ -176,6 +176,12 @@ function train_imagenet_bandit(model, data_path)
                 --             momentum = opt.momentum,
                 --             learningRateDecay = 5e-7
                 --          }
+
+                 print("probability_of_actions",probability_of_actions)
+                 print("actions",actions)
+                 print("rewards",rewards)
+                 print("targets",targets)
+
                 return inputs,actions,rewards,probability_of_actions, targets, opt.temperature, batch_number, baseline
             end --load_bandit_data,
             ,
