f3ea37af88e655556b0e6545e75a8c7d74d59ac9
Artem Grotov <agrotov@vpn-staff-146-50-70-126.vpn.uva.nl>
2016-10-26 13:15:36 +0300

    bandit

--- train_bandit.lua
+++ train_bandit.lua
@@ -174,7 +174,7 @@ function compute_target(outputs, size, actions, rewards_arg, probability_actions
 
     print("variace_regularised_target",variace_regularised_target:mean(),variace_regularised_target:min(),variace_regularised_target:max())
 
-    log_probability_of_actions_val = log_probability_of_actions(outputs, actions):float()
+    log_probability_of_actions_val = log_probability_of_actions(outputs, actions)
 --    log_probability_of_actions_val_scattered = torch.Tensor(size):fill(0)
 --    log_probability_of_actions_val_scattered:scatter(2,actions:long(),log_probability_of_actions_val:float())
 
