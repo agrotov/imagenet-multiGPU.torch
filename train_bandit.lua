04963d5ea06f58b8288adb2332cf1205a17bf682
Artem Grotov <agrotov@vpn-staff-146-50-70-126.vpn.uva.nl>
2016-10-26 12:17:47 +0300

    bandit

--- train_bandit.lua
+++ train_bandit.lua
@@ -182,6 +182,8 @@ function compute_target(outputs, size, actions, rewards_arg, probability_actions
 
     new_target = -torch.cdiv(variace_regularised_target, log_probability_of_actions_val_scattered)
 
+    print("new_target",new_target:mean(),new_target:min(),new_target:max())
+
     return new_target
 
 --    target:scatter(2,actions:long(),weight:float())
