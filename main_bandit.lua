2a238197f7264f9c98afce7bf66a4f15039f2500
Artem Grotov <agrotov@vpn-staff-146-50-70-126.vpn.uva.nl>
2016-10-26 18:21:44 +0300

    bandit

--- main_bandit.lua
+++ main_bandit.lua
@@ -261,14 +261,14 @@ function train_imagenet_bandit(model, data_path)
            print("epoch",epoch,"rewards_sum_new_train",rewards_sum_new_train,"rewards_sum_new_train - rewards_sum_logged_train",rewards_sum_new_train - rewards_sum_logged_train,"rewards_new_train",rewards_new_train,"rewards_logged_train",rewards_logged_train,"batch_number",batch_number)
            print("epoch",epoch,"rewards_sum_new_test",rewards_sum_new_test,"rewards_sum_logged_test",rewards_sum_logged_test,"rewards_sum_new_test - rewards_sum_logged_test",rewards_sum_new_test - rewards_sum_logged_test,"rewards_new_test",rewards_new_test,"rewards_logged_test",rewards_logged_test,"batch_number",batch_number)
 
-                   if rewards_sum_new_test - rewards_weigted_test_current < 0 then
-    --                   model:clearState()
-    --                   saveDataParallel(paths.concat(opt.save, 'model_' .. epoch .. '.t7'), model) -- defined in util.lua
-    --                   torch.save(paths.concat(opt.save, 'optimState_' .. epoch .. '.t7'), optimState)
-    --                   print("no improvement")
-    --                   os.exit()
-                       print("Would terminate epoch",epoch,"rewards_sum_new_test",rewards_sum_new_test,"rewards_sum_logged_test",rewards_sum_logged_test,"rewards_sum_new_test - rewards_sum_logged_test",rewards_sum_new_test - rewards_sum_logged_test,"rewards_new_test",rewards_new_test,"rewards_logged_test",rewards_logged_test)
-                   end
+--                   if rewards_sum_new_test - rewards_weigted_test_current < 0 then
+--    --                   model:clearState()
+--    --                   saveDataParallel(paths.concat(opt.save, 'model_' .. epoch .. '.t7'), model) -- defined in util.lua
+--    --                   torch.save(paths.concat(opt.save, 'optimState_' .. epoch .. '.t7'), optimState)
+--    --                   print("no improvement")
+--    --                   os.exit()
+--                       print("Would terminate epoch",epoch,"rewards_sum_new_test",rewards_sum_new_test,"rewards_sum_logged_test",rewards_sum_logged_test,"rewards_sum_new_test - rewards_sum_logged_test",rewards_sum_new_test - rewards_sum_logged_test,"rewards_new_test",rewards_new_test,"rewards_logged_test",rewards_logged_test)
+--                   end
 
            rewards_weigted_test_current = rewards_sum_new_test
        end --if
