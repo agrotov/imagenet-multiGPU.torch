c1de0401734713c9d2a5da8c95f2830ec9607c1a
Artem Grotov <agrotov@wcw-staff-145-18-162-94.wireless.uva.nl>
2016-11-03 11:46:04 +0100

    bandit

--- donkey.lua
+++ donkey.lua
@@ -59,6 +59,7 @@ local trainHook = function(self, path, h1, w1, flip)
    local oH = sampleSize[2]
    local h1 = h1 or math.ceil(torch.uniform(1e-2, iH-oH))
    local w1 = w1 or math.ceil(torch.uniform(1e-2, iW-oW))
+   print("path",path)
    local out = image.crop(input, w1, h1, w1 + oW, h1 + oH)
    assert(out:size(3) == oW)
    assert(out:size(2) == oH)
